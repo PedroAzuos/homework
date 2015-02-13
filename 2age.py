22f8f9cc3aed56531c9e99c57e24fc9b1b1bc52b
Pedro Sousa <sousaepedro@gmail.com>
2015-02-13 00:27:54 +0000

    2age updated

--- 2age.py
+++ 2age.py
@@ -4,6 +4,9 @@ dCurrentDate = datetime.datetime.now()
 
 iAge = input("Insert age")
 iMonth = input("Insert month of birth")
+
+if (iMonth>dCurrentDate.month):
+    iAge+=1
 iNewDate = dCurrentDate.year-iAge
 
 print "Born in: {}-{}".format(iMonth, iNewDate)
